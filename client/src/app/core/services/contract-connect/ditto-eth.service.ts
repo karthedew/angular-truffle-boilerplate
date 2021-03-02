971c400699eeb427347303378253780d3e035379
Karl Schmidt <karthedew@gmail.com>
2021-03-02 07:25:43 -0800

    added contract functions to this service component.

--- client/src/app/core/services/contract-connect/ditto-eth.service.ts
+++ client/src/app/core/services/contract-connect/ditto-eth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { DittoEthInjectable } from '../../injectables/contract-injectables/ditto-eth.injectable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DittoEthService {
 
-  constructor() { }
+  constructor(
+    private dittoEthInjectable: DittoEthInjectable
+  ) {
+    // 42 is the Kovan Network
+    // 7776 is the Truffle Network
+  }
+
+  async name(): Promise<string> {
+    return await this.dittoEthInjectable.name();
+  }
+
+  async balanceOf(addrs:string): Promise<string> {
+    return await this.dittoEthInjectable.addressOf(addrs);
+  }
+
+  // deposit()
+
+
+  // widthdraw()
+
+
+  // balanceOf()
 }
