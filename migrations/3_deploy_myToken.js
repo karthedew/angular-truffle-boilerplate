376037d3d724200419b93ddbbe83adbb1d083151
Karl Schmidt <karthedew@gmail.com>
2021-03-02 07:27:12 -0800

    unnecessary updates.

--- migrations/3_deploy_myToken.js
+++ migrations/3_deploy_myToken.js
@@ -2,4 +2,8 @@ const DittoEth = artifacts.require("tokens/DittoEth")
 
 module.exports = function(deployer) {
   deployer.deploy(DittoEth);
+
+  // We want to store the deployed contract address
+  // into the appropirate Angular environment 
+  // file.
 };
