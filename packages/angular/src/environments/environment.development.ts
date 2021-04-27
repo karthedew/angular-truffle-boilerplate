201ff368b9751ffb878adf0f73b833b7f0da5fb2
Karl Schmidt <35015336+karthedew@users.noreply.github.com>
2021-04-27 07:30:45 -0700

    Delete packages/angular/src/environments directory

--- packages/angular/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    hostURL: 'http://localhost',
-    INFURA_ID: 'b8bdd09e0e3641279406dde31619dda0'
-  };
--- packages/angular/src/environments/environment.kovan.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    kovan: true,
-    hostURL: 'http://localhost',
-    INFURA_ID: 'b8bdd09e0e3641279406dde31619dda0'
-  };
